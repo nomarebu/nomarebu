4f6da20e278f504d3323f5d9d77f1c62ceeb7f66
kenchan <kenichi.taka@gmail.com>
2010-01-26 19:24:42 +0900

    use sequel

--- nomarebu_bot.rb
+++ nomarebu_bot.rb
@@ -2,8 +2,16 @@
 
 require 'rubygems'
 require 'rubytter'
+require 'sequel'
 
 account = YAML.load_file(File.dirname(__FILE__) + "/account.yml")
 c = Rubytter.new(account['nomarebu']['login'], account['nomarebu']['password'])
-s = File.read(File.join(File.dirname(__FILE__), "sake.txt")).split
-c.update(s[rand(s.size)])
+DB = Sequel.connect('sqlite://my.db')
+
+DB.create_table :sakes do
+  primary_key :id
+  String :brand
+end unless DB.table_exists?(:sakes)
+
+sakes = DB[:sakes]
+c.update(:post, sakes[:id => rand(sakes.count)]) unless sakes.count == 0
