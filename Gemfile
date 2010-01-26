bea8f81bc5bb785a90b39a8daefc5bda71672f93
kenchan <kenichi.taka@gmail.com>
2010-01-27 00:28:32 +0900

    use bundler

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'http://gemcutter.org'
 
 gem 'sinatra', :require_as => 'sinatra/base'
 gem 'haml', :require_as => %w(haml sass)
+gem 'rubytter'
+gem 'sequel'
--- nomarebu_bot.rb
+++ nomarebu_bot.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'rubytter'
-require 'sequel'
+require File.join(File.dirname(__FILE__), 'vendor', 'gems', 'environment')
+Bundler.require_env ENV['RACK_ENV']
 
 account = YAML.load_file(File.dirname(__FILE__) + "/account.yml")
 c = Rubytter.new(account['nomarebu']['login'], account['nomarebu']['password'])
